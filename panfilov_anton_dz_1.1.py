14981876a18c27d8053e266c8d102e0954030dde
Anton140693 <80172086+Anton140693@users.noreply.github.com>
2021-04-07 07:48:47 +0300

    Initial commit

---
+++ panfilov_anton_dz_1.1.py
@@ -0,0 +1,27 @@
+# Вариант a)
+duration = int(input('Введите количество секунд: '))
+s = str(duration)
+print(s + ' сек ')
+
+# Вариант b)
+duration = int(input('Введите количество секунд: '))
+m = str((duration //60) % 60)
+s = str(duration % 60)
+print(m + ' мин ' + s + ' сек ')
+
+
+# Вариант c)
+duration = int(input('Введите количество секунд: '))
+h = str(duration // 3600)
+m = str((duration //60) % 60)
+s = str(duration % 60)
+print(h + ' час ' + m + ' мин ' + s + ' сек ')
+
+
+# Вариант d**)
+duration = int(input('Введите количество секунд: '))
+d = str(duration // 86400)
+h = str((duration // 3600) % 24)
+m = str((duration //60) % 60)
+s = str(duration % 60)
+print(d + ' дн ' + h  + ' час ' + m + ' мин ' + s + ' сек ')
